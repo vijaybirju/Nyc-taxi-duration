12276946763614a53d2c5e9062b731be735ab218
vijaybirju <Vijaykumar14198@gmail.com>
2025-02-15 16:02:47 +0530

    Write train_val_split function for spliting dataset

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -24,7 +24,18 @@ def load_raw_data(input_path: Path) -> pd.DataFrame:
     return raw_data
 
 
-
+def train_val_split(data: pd.DataFrame,
+                    test_size: float,
+                    random_state: int) -> tuple[pd.DataFrame, pd.DataFrame]:
+    train_data, val_data = train_test_split(data,
+                                            test_size=test_size,
+                                            random_state=random_state)
+    dataset_logger.save_logs(msg = f"Data is split in train and val with shapes { train_data.shape} and {val_data.shape} respectively",
+                             log_level = 'info')
+    dataset_logger.save_logs(msg = f"The Parametres values are { test_size} for test_size and { random_state} for random_state",
+                             log_leve = 'info')
+    
+    return train_data, val_data
 
 
 
