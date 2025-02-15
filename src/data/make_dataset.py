ec77f489c0f86c51a8dd2a4aebe257f34679b0d3
vijaybirju <Vijaykumar14198@gmail.com>
2025-02-15 15:48:48 +0530

    Write load_raw_data function to load data

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -7,3 +7,24 @@ from sklearn.model_selection import train_test_split
 from src.logger import create_log_path, CustomLogger
 
 
+log_file_path = create_log_path('make_dataset')
+# create custom logger object
+
+dataset_logger = CustomLogger(logger_name='make_dataset',
+                              log_filename=log_file_path)
+
+# set the logging level info
+dataset_logger.set_log_level(level=logging.INFO)
+
+def load_raw_data(input_path: Path) -> pd.DataFrame:
+    raw_data = pd.read_csv(input_path)
+    rows, columns = raw_data.shape
+    dataset_logger.save_logs(msg=f'{input_path.stem} data read having { rows} rows and {columns} columns'
+                             , log_level='info')
+    return raw_data
+
+
+
+
+
+
