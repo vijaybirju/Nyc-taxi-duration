8170b0cbfc91f387d8f8ad8b946928445de5010b
vijaybirju <Vijaykumar14198@gmail.com>
2025-02-24 16:45:55 +0530

    Added various distance formula in distance.py

--- src/features/distances.py
+++ src/features/distances.py
@@ -2,7 +2,11 @@ import numpy as np
 
 
 def haversine_distance(lat1: float, lon1: float, lat2: float, lon2: float) -> float:
-    r = 6371  # Radius of earth
+    """
+    Calculate haversine distances between two points given their latitude and
+    longitude coordinates
+    """
+    R = 6371  # Radius of earth
     
     lat1, lon1, lat2, lon2 = map(np.radians, [lat1, lon1, lat2, lon2])
 
@@ -11,12 +15,38 @@ def haversine_distance(lat1: float, lon1: float, lat2: float, lon2: float) -> fl
 
     a = (np.sin(dlat/2))**2 + np.cos(lat1)*np.cos(lat2)*(np.sin(dlon/2))**2
 
-    d = 2*r*np.arcsin(np.sqrt(a))
+    d = 2*R*np.arcsin(np.sqrt(a))
 
     return d
 
-lat1, lon1 = 28.7041, 77.1025  # Delhi
-lat2, lon2 = 19.0760, 72.8777  # Mumbai
 
-distance = haversine_distance(lat1, lon1, lat2, lon2)
-print(f"Haversine Distance: {distance:.2f} km")
+def euclidean_distance(lat1: float, lon1: float, lat2: float, lon2: float) -> float:
+    """
+    Calculate euclidean distances between two points given their latitude and
+    longitude coordinates
+    """
+    r = 111  # Approximate km per degree
+    dlon = (lon2 - lon1)*r*np.cos(np.radians((lat1 + lat2) / 2)) 
+    dlat = (lat2 - lat1)*r
+
+    a = dlon**2 + dlat**2
+
+    d = np.sqrt(a)
+
+    return d
+
+def manhattan_distance(lat1: float, lon1: float, lat2: float, lon2: float) -> float:
+    """
+    Calculate manhattan distances between two points given their latitude and
+    longitude coordinates
+    """
+    r = 111  # Approximate km per degree
+    dlon = (lon2 - lon1)*r*np.cos(np.radians((lat1 + lat2) / 2)) 
+    dlat = (lat2 - lat1)*r
+
+    d = np.abs(dlon) + np.abs(dlat)
+
+    return d
+
+
+
