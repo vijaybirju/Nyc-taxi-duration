f067fcef9833e7da648dcba24c932acec898f34f
vijaybirju <Vijaykumar14198@gmail.com>
2025-03-04 16:30:40 +0530

    write outliers_removal file

---
+++ src/features/outliers_removal.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+from sklearn.base import BaseEstimator, TransformerMixin, OneToOneFeatureMixin
+
+class OutlierRemover(TransformerMixin,OneToOneFeatureMixin, BaseEstimator):
+
+    def __init__(self, percentile_values:list,col_subset:list):
+        self.percentile_value = percentile_values
+        self.col_subset = col_subset
+
+    def fit(self, X, y = None):
+        # make a copy of X
+        X = X.copy()
+        
+
+        self.quantiles = []
+
+        for col in self.col_subset:
+            lower_bound = X.loc[:,col].quantile(q=self.percentile_values[0])
+            upper_bound = X.loc[:,col].quantile(q=self.percentile_values[1])
+            
+            self.quantiles_.append((lower_bound,upper_bound))
+
+        return self 
+    
+def tranform(self, X):
+    X = X.copy()
+
+    for ind,col in enumerate(self.col_subset):
+        lower_bound, upper_bound = self.quantiles_[ind]
+        filter_df = X[(X.loc[:,col] >= lower_bound) & (X.loc[:,col] <= upper_bound)]
+        X = filter_df
+            
+    return X
