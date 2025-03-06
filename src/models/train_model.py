1f15c11c2ae20ce4e3956ad4dec2597cc8f3b84e
vijaybirju <Vijaykumar14198@gmail.com>
2025-03-06 16:37:39 +0530

    Done with training completed training file

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -31,4 +31,32 @@ def train_model(model, X_train, y_train):
 def save_model(model, save_path):
     joblib.dump(value=model,
                 filename=save_path)
+    
+
+def main():
+    # current file path
+    current_path = Path(__file__)
+    # root path
+    root_path = current_path.parent.parent.parent
+    # training dat path
+    training_data_path = root_path / sys.argv[1]
+    # load training data into dataframe
+    train_data = load_dataframe(training_data_path)
+    # make X and y from training data 
+    X_train, y_train = make_x_y(train_data,TARGET)
+    # make the model object
+    regressor = XGBRegressor()
+    # train the model
+    regressor = train_model(model=regressor,
+                            X_train=X_train,
+                            y_train=y_train)
+    # save the model after training 
+    model_output_path = root_path / 'models' / 'models'
+    model_output_path.mkdir(exist_ok=True)
+    save_model(model=regressor,save_path=model_output_path / 'xgbreg.joblib')
+
+
+if __name__ == '__main__':
+    main()
+    
 
